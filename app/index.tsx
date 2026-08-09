a41d862eb5e5c4fd5342324a59837d525dd4e45e
Rolie-CODE <rytenkorang@st.ug.edu.gh>
2026-08-08 21:39:47 -0700

    commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,112 @@
-import { View, Text, TouchableOpacity } from 'react-native';
-import { useRouter } from 'expo-router';
+import {View,Text, TouchableOpacity} from 'react-native';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import EvilIcons from '@expo/vector-icons/EvilIcons';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+
 
 export default function HomeScreen() {
-  const router = useRouter();
 
-  return (
-    <View style={{ flex: 1, backgroundColor: "white" }}>
-      <Text>Hello this is the Homescreen</Text>
+  return(
 
-      <TouchableOpacity
-        onPress={() => router.push('/depression_screen')}
-        style={{
-          backgroundColor: 'green',
-          padding: 12,
-          borderRadius: 5,
-          alignItems: 'center',
-          margin: 20,
-        }}
-      >
-        <Text style={{ color: 'white', fontWeight: 'bold' }}>
-          Depression Screen
+    <View style = {{flex:1, backgroundColor: 'white',}}>
+      <View style = {{backgroundColor: 'green', height: 120}}>
+        <View style = {{flexDirection : 'row', gap: 80, alignItems: 'center', justifyContent: 'center'}}>
+          <View>
+            <Text style = {{fontSize: 30, color: 'white'}}>
+              Analyze Me
+            </Text>
+            <Text style = {{color: 'white'}}>
+              What would you like to analyze today?
+            </Text>
+          </View>
+          <AntDesign name="picture" size={24} color="white" style = {{}}/>
+        </View>
+        <TouchableOpacity style = {{flexDirection: 'row', borderWidth: 0.5, borderRadius: 0.5, width: 290, marginLeft: 35, gap: 10}}>
+            <EvilIcons name="search" size={24} color="black" style={{marginLeft: 5}}/>
+            <Text>
+              Search analyses...
+            </Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style = {{backgroundColor: 'black', flexDirection: 'row', gap: 12}}>
+
+        <View>
+          <Text style = {{color: 'white'}}>
+            Analyses run
+          </Text>
+
+          <Text style = {{color: 'white'}}>
+            12
+          </Text>
+
+          <Text style = {{color: 'white'}}>
+            all time
+          </Text>
+        </View>
+
+        <View>
+          <Text style = {{color: 'white'}}>
+            Last result
+          </Text>
+
+          <Text style = {{color: 'white'}}>
+            Low Risk
+          </Text>
+
+          <Text style = {{color: 'white'}}>
+            depression
+          </Text>
+        </View>
+      </View>
+
+      <View style = {{backgroundColor: 'black'}}>
+
+        <Text style = {{color: 'white'}}>
+          Available Modules
         </Text>
-      </TouchableOpacity>
+
+        <View>
+          <View>
+            <FontAwesome5 name="brain" size={24} color="white" />
+            <Text style = {{color: 'white'}}>
+              Depression risk
+            </Text>
+            <Text style = {{color: 'white'}}>
+              Sleep, stress and lifestyle
+            </Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <FontAwesome5 name="brain" size={24} color="white" />
+            <Text style = {{color: 'white'}}>
+              Loan egibility
+            </Text>
+            <Text style = {{color: 'white'}}>
+              Income, credit and history
+            </Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <FontAwesome5 name="brain" size={24} color="white" />
+            <Text style = {{color: 'white'}}>
+              More Coming
+            </Text>
+            <Text style = {{color: 'white'}}>
+              New Modules in development
+            </Text>
+            <Text>
+              Soon
+            </Text>
+          </View>
+        </View>
+      </View>
     </View>
-  );
+
+  )
+
 }
